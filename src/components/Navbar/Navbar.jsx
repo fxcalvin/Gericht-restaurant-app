7d0f5bac8387da7854ea60c134b092c15a271efd
calvinogwang <136588665+calvinogwang@users.noreply.github.com>
2023-11-16 01:34:56 +0300

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useState } from 'react'; 
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import images from '../../constants/images';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  return(
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht } alt='app logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href='#home'>Home</a></li>
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#contacts'>Contacts</a></li>
+      </ul>
+      <div className='app__navbar-login'>
+        <a href='#login' className='p__opensans'>Login / Register</a>
+        <div /> 
+        <a href='/' className='p__opensans'>Book table</a>
+        
+      </div>
+      <div className='app__navbar-smallscreens'>
+        <GiHamburgerMenu color='#ffff' fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+          <div className='app__navbar-smallscreens_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)}></MdOutlineRestaurantMenu>
+            <ul className='app__navbar-smallscreens_links'>
+              <li className='p__opensans'><a href='#home'>Home</a></li>
+              <li className='p__opensans'><a href='#about'>About</a></li>
+              <li className='p__opensans'><a href='#menu'>Menu</a></li>
+              <li className='p__opensans'><a href='#awards'>Awards</a></li>
+              <li className='p__opensans'><a href='#contacts'>Contacts</a></li>
+            </ul>
+          </div>   
+        )}
+
+
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
